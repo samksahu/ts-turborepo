4d0c04b68e7dd818286192c194181d0976d6769e
Shambhu Sahu <shambhukumar.shau@gmail.com>
2022-11-22 09:20:49 +0530

    initial commit

---
+++ packages/ui/dist/index.mjs
@@ -0,0 +1,11 @@
+// src/Button.tsx
+import { jsx } from "react/jsx-runtime";
+var Button = () => {
+  return /* @__PURE__ */ jsx("button", {
+    className: "text-red-600 border-4 border-black",
+    children: "sBoop"
+  });
+};
+export {
+  Button
+};
---
+++ packages/ui/src/Button.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+export const Button = () => {
+  return <button className="text-red-600 border-4 border-black">sBoop</button>;
+};
